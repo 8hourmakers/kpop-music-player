133880cc5e45114b1aacd3c80d2604d07103ae57
bdh1011 <bdh931101@gmail.com>
2017-03-12 17:42:45 +0900

    Upload lambda-server

---
+++ lambda-server/js/audioAssets.js
@@ -0,0 +1,86 @@
+'use strict';
+
+var audioData = [
+    {
+        title: '1. BU DAM IE DOEI (Feat. HUI IN of MA-MA-MU) - JUNG KIE',
+        url: 'https://kpop-musics-for-alexa.surge.sh/2017-03-03/1.mp3'
+    },
+    {
+        title: '2. KNOCK KNOCK - TWICE',
+        url: 'https://kpop-musics-for-alexa.surge.sh/2017-03-03/2.mp3'
+    },
+    {
+    	title: '3. Fine - TAEYEON',
+    	url: 'https://kpop-musics-for-alexa.surge.sh/2017-03-03/3.mp3'
+    },
+    {
+    	title: '4. Chut noon chue rum, Neo Eo Gae Ga Get Da - Alelly',
+    	url: 'https://kpop-musics-for-alexa.surge.sh/2017-03-03/4.mp3'
+    },
+    {
+    	title: '5. Rookie - Red Velvet',
+    	url: 'https://kpop-musics-for-alexa.surge.sh/2017-03-03/5.mp3'
+    },
+    {
+    	title: '6. BOM NAL - BANG TAN SO NYUN DAN',
+    	url: 'https://kpop-musics-for-alexa.surge.sh/2017-03-03/6.mp3'
+    },
+    {
+    	title: '7. JO TA GO MAL HAE - BOL BBAL GAN SA CHOON GIE',
+    	url: 'https://kpop-musics-for-alexa.surge.sh/2017-03-03/7.mp3'
+    },
+    {
+    	title: '8. YESTERDAY - Block B',
+    	url: 'https://kpop-musics-for-alexa.surge.sh/2017-03-03/8.mp3'
+    },
+    {
+    	title: '9. MOVIE - B TO B',
+    	url: 'https://kpop-musics-for-alexa.surge.sh/2017-03-03/9.mp3'
+    },
+    {
+    	title: '10. Rain - SO YOU, BAEKHYUN',
+    	url: 'https://kpop-musics-for-alexa.surge.sh/2017-03-03/10.mp3'
+    },
+    {
+    	title: '11. Marry Me - MAKTUB, GOO YOON HEE',
+    	url: 'https://kpop-musics-for-alexa.surge.sh/2017-03-03/11.mp3'
+    },
+    {
+    	title: '12. OH RAN NAL OH RAN BAM - ACC DONG MU JI SION',
+    	url: 'https://kpop-musics-for-alexa.surge.sh/2017-03-03/12.mp3'
+    },
+    {
+    	title: '13. Beautiful - Crush',
+    	url: 'https://kpop-musics-for-alexa.surge.sh/2017-03-03/13.mp3'
+    },
+    {
+    	title: '14. NO RAE - Zion.T',
+    	url: 'https://kpop-musics-for-alexa.surge.sh/2017-03-03/14.mp3'
+    },
+    {
+    	title: '15. OH PPA YA - SINHYUN HEE WA KIM ROOT',
+    	url: 'https://kpop-musics-for-alexa.surge.sh/2017-03-03/15.mp3'
+    },
+    {
+    	title: '16. FINGERTIP - YEO JA CHIN GU',
+    	url: 'https://kpop-musics-for-alexa.surge.sh/2017-03-03/16.mp3'
+    },
+    {
+    	title: '17. Stay With Me - CHANYEOL, Punch',
+    	url: 'https://kpop-musics-for-alexa.surge.sh/2017-03-03/17.mp3'
+    },
+    {
+    	title: '18. TT - TWICE',
+    	url: 'https://kpop-musics-for-alexa.surge.sh/2017-03-03/18.mp3'
+    },
+    {
+    	title: '19. I CANNOT FIND MY HOME(Feat. BOL BBAL GAN SA CHUN GI) - MAD CLOWN',
+    	url: 'https://kpop-musics-for-alexa.surge.sh/2017-03-03/19.mp3'
+    },
+    {
+    	title: '20. EHH RA MO RE GET DA - BIGBANG',
+    	url: 'https://kpop-musics-for-alexa.surge.sh/2017-03-03/20.mp3'
+    }
+];
+
+module.exports = audioData;
